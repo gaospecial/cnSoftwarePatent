73795b2240f3e930c6e6abb6528b7758c040d630
Chun-Hui Gao <gaospecial@gmail.com>
2021-05-18 11:20:04 +0800

    first commit

---
+++ R/code.R
@@ -0,0 +1,24 @@
+code <- function (path = ".", ignore = ".Rbuildignore", pattern = "(R|md|Rd|Rmd|DESCRIPTION|NAMESPACE)$"){
+  files = list.files(path = path, recursive = TRUE, full.names = TRUE, pattern = pattern)
+  ignore = readLines(ignore)
+  ignore = gsub("#.*","",ignore)
+  pat = paste0(ignore, collapse = "|")
+  files = files[grep(pat, files, invert = TRUE)]
+  success = lapply(files, function(x){
+    paste("##", x) %>% cat()
+    cat("\n\n")
+    import_example(x) %>% cat()
+    cat("\n\n")
+  })
+  cat("\n\nAll source codes are exported successfully. \n")
+}
+
+import_example <- function(file, lang = xfun::file_ext(file)) {
+  x = xfun::read_utf8(xfun::magic_path(file))
+  lang = tolower(lang)
+  if (nchar(lang) > 1) {
+    lang = sub('^r', '', lang)
+    if (lang == 'nw') lang = 'tex'
+  }
+  knitr::asis_output(paste(c(sprintf("````%s", lang), x, "````"), collapse = '\n'))
+}
